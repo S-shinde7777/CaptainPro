86245d14eea7cc886dc41d8e2b6efb9c17ffdf33
Sagar Shinde <sagarshindecse708@gmail.com>
2026-08-04 15:48:38 +0530

    admin navbar fixed

--- Client-Side/src/components/Admin/AdminNavbar.jsx
+++ Client-Side/src/components/Admin/AdminNavbar.jsx
@@ -1,16 +1,13 @@
-import {
-  Database,
-  DollarSign,
-  User,
-  Settings,
-} from "lucide-react";
+import { Database, DollarSign, User, Settings } from "lucide-react";
+import { NavLink } from "react-router-dom";
+
+
 
 function AdminNavbar() {
+
   return (
     <header className="bg-[#1B2340] border-b border-slate-700">
-
       <div className="max-w-screen-2xl mx-auto px-6 h-20 flex items-center justify-between">
-
         {/* Logo */}
 
         <div>
@@ -18,57 +15,76 @@ function AdminNavbar() {
             Captain<span className="text-orange-500">Pro</span>
           </h1>
 
-          <p className="text-gray-300 text-lg">
-            Dashboard
-          </p>
+          <p className="text-gray-300 text-lg">Dashboard</p>
         </div>
 
         {/* Menu */}
 
-        <div className="flex items-center gap-12">
-
-          <button className="flex flex-col items-center text-gray-300 hover:text-white duration-300">
+        <div 
+         to="/master-data"
+        className="flex items-center gap-12">
+          <NavLink className={({ isActive }) =>
+              ` flex flex-col items-center ${
+                isActive
+                  ? "bg-[#1B2340] text-orange-500"
+                  : "text-gray-300 hover:bg-[#1B2340] hover:text-white"
+              }`
+            }>
             <Database size={26} />
             <span className="text-sm mt-1">Master Data</span>
-          </button>
+          </NavLink>
 
-          <button className="flex flex-col items-center text-orange-500">
+          <NavLink
+           to="/master-data"
+           className={({ isActive }) =>
+              `flex flex-col items-center ${
+                isActive
+                  ? "bg-[#1B2340] text-orange-500"
+                  : "text-gray-300 hover:bg-[#1B2340] hover:text-white"
+              }`
+            }
+          >
             <DollarSign size={26} />
             <span className="text-sm mt-1">Sale</span>
-          </button>
+          </NavLink>
 
-          <button className="flex flex-col items-center text-gray-300 hover:text-white duration-300">
+          <NavLink 
+           to="/master-data"
+          className={({ isActive }) =>
+              `flex flex-col items-center ${
+                isActive
+                  ? "bg-[#1B2340] text-orange-500"
+                  : "text-gray-300 hover:bg-[#1B2340] hover:text-white"
+              }`
+            }>
             <User size={26} />
             <span className="text-sm mt-1">Account</span>
-          </button>
+          </NavLink>
 
-          <button className="flex flex-col items-center text-gray-300 hover:text-white duration-300">
+          <NavLink
+            to="/master-data"
+           className={({ isActive }) =>
+              `flex flex-col items-center ${
+                isActive
+                  ? "bg-[#1B2340] text-orange-500"
+                  : "text-gray-300 hover:bg-[#1B2340] hover:text-white"
+              }`
+            }>
             <Settings size={26} />
             <span className="text-sm mt-1">Settings</span>
-          </button>
-
+          </NavLink>
         </div>
 
         {/* Right Side */}
 
         <div className="text-right">
+          <p className="text-gray-300 text-sm">08/07/2026</p>
 
-          <p className="text-gray-300 text-sm">
-            08/07/2026
-          </p>
+          <p className="text-gray-500 text-xs">03:24 PM</p>
 
-          <p className="text-gray-500 text-xs">
-            03:24 PM
-          </p>
-
-          <p className="text-white font-semibold mt-1">
-            Admin
-          </p>
-
+          <p className="text-white font-semibold mt-1">Admin</p>
         </div>
-
       </div>
-
     </header>
   );
 }
